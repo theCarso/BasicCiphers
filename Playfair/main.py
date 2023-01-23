24f2a9c67bdaeae1337e0940e14bdfd31c4618d9
theCarso <carso.miller21@gmail.com>
2023-01-22 21:05:29 -0500

    Added pair encryption

--- Playfair/main.py
+++ Playfair/main.py
@@ -19,10 +19,43 @@ def loadMatrix(key):
             letters.remove(character)
     # loop through remaining characters alphabetically
     for character in letters:
-        print(character)
         matrix[i][j] = character
         i+=1
         if i >= 5:
             i=0
             j+=1
     return matrix
+
+# returns index as (x,y)
+def findInMatrix(matrix, character):
+    for i, x in enumerate(matrix):
+        if character in x:
+            return (i, x.index(character))
+
+# returns a 2 character encrypted string
+def encryptPair(matrix, pair):
+    pair=pair.upper().replace("J", "I")
+
+    firstCharIndex = findInMatrix(matrix, pair[0])
+    secondCharIndex = findInMatrix(matrix, pair[1])
+
+    # Same row check
+    if firstCharIndex[1] == secondCharIndex[1]:
+        firstEnc = matrix[(firstCharIndex[0]+1)%5][firstCharIndex[1]]
+        secondEnc = matrix[(secondCharIndex[0]+1)%5][secondCharIndex[1]]
+        return firstEnc + secondEnc
+
+    # Same column check
+    if firstCharIndex[0] == secondCharIndex[0]:
+        firstEnc = matrix[(firstCharIndex[0])][(firstCharIndex[1]+1 % 5)]
+        secondEnc = matrix[(secondCharIndex[0])][(secondCharIndex[1]+1) % 5]
+        return firstEnc + secondEnc
+    
+    # Different row and column
+    firstEnc = matrix[secondCharIndex[0]][firstCharIndex[1]]
+    secondEnc = matrix[firstCharIndex[0]][secondCharIndex[1]]
+    return firstEnc + secondEnc
+    
+
+
+matrix = loadMatrix('MONARCHY')
