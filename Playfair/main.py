3712b8bd174d749984a210ee2888309e6e03534e
Carson Miller <miller.1653@wright.edu>
2023-01-22 21:22:31 -0500

    Added decryption and user input to Playfair

--- Playfair/main.py
+++ Playfair/main.py
@@ -1,5 +1,7 @@
 import string
 
+filler = 'X'
+
 def loadMatrix(key):
     # setup matrix and character list
     key = key.upper()
@@ -56,6 +58,66 @@ def encryptPair(matrix, pair):
     secondEnc = matrix[firstCharIndex[0]][secondCharIndex[1]]
     return firstEnc + secondEnc
     
+def encryptString(matrix, str):
+    str=str.upper()
+    i = 0
+    result = ''
+    while i < len(str):
+        if i+1 >= len(str):
+            result = result + encryptPair(matrix, str[i] + filler)
+            i+=1
+        elif str[i] == str[i+1]:
+            result = result + encryptPair(matrix, str[i] + filler)
+            i+=1
+        else:
+            result = result + encryptPair(matrix, str[i] + str[i+1])
+            i+=2
+    return result
+
+def decryptPair(matrix, pair):
+    firstCharIndex = findInMatrix(matrix, pair[0])
+    secondCharIndex = findInMatrix(matrix, pair[1])
+
+    # Same row check
+    if firstCharIndex[1] == secondCharIndex[1]:
+        firstEnc = matrix[(firstCharIndex[0]-1)%5][firstCharIndex[1]]
+        secondEnc = matrix[(secondCharIndex[0]-1)%5][secondCharIndex[1]]
+        return firstEnc + secondEnc
+
+    # Same column check
+    if firstCharIndex[0] == secondCharIndex[0]:
+        firstEnc = matrix[(firstCharIndex[0])][(firstCharIndex[1]-1 % 5)]
+        secondEnc = matrix[(secondCharIndex[0])][(secondCharIndex[1]-1) % 5]
+        return firstEnc + secondEnc
+    
+    # Different row and column
+    firstEnc = matrix[secondCharIndex[0]][firstCharIndex[1]]
+    secondEnc = matrix[firstCharIndex[0]][secondCharIndex[1]]
+    return firstEnc + secondEnc
+
+def decryptString(matrix, str):
+    i = 0
+    result = ''
+    while i < len(str):
+        result = result + encryptPair(matrix, str[i] + str[i+1])
+        i+=2
+    return result.lower()
 
 
-matrix = loadMatrix('MONARCHY')
+while True:
+    prompt = input('Encrypt or decrypt (e/d)? ')
+    # decrypt the text
+    if(prompt.lower() == 'd'):
+        key = input('Input your key: ').upper()
+        matrix = loadMatrix(key)
+        ciphertext = input('Input your ciphertext: ')
+        print(ciphertext.upper() + " {" + key + "} -> " + decryptString(matrix, ciphertext))
+    # encrypt the text
+    elif (prompt.lower() == 'e'):
+        key = input('Input your key: ').upper()
+        matrix = loadMatrix(key)
+        plaintext = input('Input your plaintext: ')
+        print(plaintext.lower() + " {" + key + "} -> " + encryptString(matrix, plaintext))
+    # invalid input
+    else:
+        print('Invalid input.')
